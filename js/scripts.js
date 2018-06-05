f6f5fba9ac9d4d33a353c9cb261ca0e62320ec02
Matt Staudacher and Steven Colburn <matt@>
2018-06-05 16:06:22 -0700

    add branching for luck and unluck

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,58 @@
+var luckyValues = [];
+var unluckyValues = [];
+var outputFortune = [];
+
 $(function(){
-  
+  $("#fortune-survey").submit(function(event){
+    event.preventDefault();
+    $("#fortune").show();
+    luckyValues = [];
+    unluckyValues = [];
+    outputFortune = [];
+    $("input:checkbox[name=signs-unlucky]:checked").each(function(){
+      unluckyValues.push($(this).val());
+    });
+    $("input:checkbox[name=signs-lucky]:checked").each(function(){
+      luckyValues.push($(this).val());
+    });
+    $("#output").text("");
+
+    var unluckyLength = unluckyValues.length;
+    var luckyLength = luckyValues.length;
+
+    //Branching for Fortune Quiz Starts Here
+    if(compareTo(luckyLength,unluckyLength))
+    {
+      outputFortune = ["Neutral"];
+    }
+    else if(luckyLength > unluckyLength)
+    {
+      outputFortune = ["Lucky"];
+    }
+    else {
+      outputFortune = ["Unlucky"];
+    }
+    //Branching for Fortune Quiz Ends Here
+    arrayWrapper(outputFortune);
+    $("#output").html(outputFortune);
+  });
 });
+
+function listWrapper(stringWrap)
+{
+  return ("<li>"+stringWrap+"</li>");
+}
+
+//Passing the pointer to an array and then running a for loop to wrap each string in the array with the function listWrapper.
+function arrayWrapper(arrayTemp)
+{
+  for(var index = 0; index < arrayTemp.length; index++)
+  {
+    arrayTemp[index] = listWrapper(arrayTemp[index]);
+  }
+}
+
+function compareTo(first,second)
+{
+  return (first === second);
+}
